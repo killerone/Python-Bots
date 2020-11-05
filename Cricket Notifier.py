9c539653fc07f4cf9c126e31cd4ce122b7c48486
Himanshu <himanshupednekar036@gmail.com>
2020-11-05 09:26:21 +0530

    bug fix

--- Cricket Notifier.py
+++ Cricket Notifier.py
@@ -9,73 +9,95 @@ options.add_argument('--incognito')
 options.add_argument("--test-type")
 options.add_argument('--headless')
 options.add_argument('--silent')
-driver = webdriver.Chrome("chromedriver.exe", options=options)
+driver = webdriver.Chrome(options=options)
 
 toast = ToastNotifier()
 
 url = "https://www.cricbuzz.com/"
 driver.get(url)
 
+
 driver.find_element_by_xpath(
     "/html/body/div[1]/div[2]/div[3]/div/div[1]/div/div[2]/ul/li[1]/a/div[2]").click()
 
-title = driver.find_element_by_xpath(
-    "/html/body/div[1]/div[2]/div[4]/div[2]/h1").text
-title = title.split(",")[0]
+try:
+    title = driver.find_element_by_xpath(
+        "/html/body/div[1]/div[2]/div[4]/div[2]/h1").text
+    title = title.split(",")[0]
 
-while 1:
-    score = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/h2").text
+    while 1:
+        score = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/h2").text
 
-    crr = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/span[1]/span[2]").text
+        try:
+            opponent = driver.find_element_by_xpath(
+                "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[1]/div[1]/h2").text
+            rrr = driver.find_element_by_xpath(
+                "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/span[2]/span[2]").text
+        except Exception:
+            pass
 
-    # Batsman 1 info
-    batsman1name = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/a").text
-    batsman1score = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[2]").text
-    batsman1balls = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]").text
-    batsman1four = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[4]").text
-    batsman1six = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[5]").text
-    batsman1sr = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[6]").text
+        crr = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/span[1]/span[2]").text
 
-    # Batsman 2 info
-    try:
-        batsman2name = driver.find_element_by_xpath(
-            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[1]/a").text
-        batsman2score = driver.find_element_by_xpath(
-            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[2]").text
-        batsman2balls = driver.find_element_by_xpath(
-            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[3]").text
-        batsman2four = driver.find_element_by_xpath(
-            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[4]").text
-        batsman2six = driver.find_element_by_xpath(
-            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[5]").text
-        batsman2sr = driver.find_element_by_xpath(
-            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[6]").text
-    except Exception:
-        batsman2name = "OUT"
-        batsman2score = batsman2balls = batsman2four = batsman2six = batsman2sr = 0
+        # Batsman 1 info
+        batsman1name = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/a").text
+        batsman1score = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[2]").text
+        batsman1balls = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[3]").text
+        batsman1four = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[4]").text
+        batsman1six = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[5]").text
+        batsman1sr = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[2]/div[6]").text
 
-    # Bowler info
-    bowlername = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/a").text
-    bowlerover = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[2]").text
-    bowlerrun = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[4]").text
-    bowlerwicket = driver.find_element_by_xpath(
-        "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[5]").text
+        # Batsman 2 info
+        try:
+            batsman2name = driver.find_element_by_xpath(
+                "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[1]/a").text
+            batsman2score = driver.find_element_by_xpath(
+                "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[2]").text
+            batsman2balls = driver.find_element_by_xpath(
+                "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[3]").text
+            batsman2four = driver.find_element_by_xpath(
+                "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[4]").text
+            batsman2six = driver.find_element_by_xpath(
+                "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[5]").text
+            batsman2sr = driver.find_element_by_xpath(
+                "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[1]/div[3]/div[6]").text
+        except Exception:
+            batsman2name = "OUT"
+            batsman2score = batsman2balls = batsman2four = batsman2six = batsman2sr = 0
 
-    body = f"{score}    Crr: {crr}\n{batsman1name}\t{batsman1score} ({batsman1balls} | {batsman1four} | {batsman1six} | {batsman1sr})"
-    body += f"\n{batsman2name}\t{batsman2score} ({batsman2balls} | {batsman2four} | {batsman2six} | {batsman2sr})"
-    body += f"\n{bowlername}\t{bowlerover} ({bowlerrun}/{bowlerwicket})"
-    # body = score + "  CRR:" + crr
-    # body += "\n"+batsman1name+"\t"+batsman1score+"("
-    toast.show_toast(title, body, duration=7)
-    time.sleep(13)
+        # Bowler info
+        bowlername = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[1]/a").text
+        bowlerover = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[2]").text
+        bowlerrun = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[4]").text
+        bowlerwicket = driver.find_element_by_xpath(
+            "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[2]/div[2]/div[2]/div[5]").text
+
+        try:
+            target = driver.find_element_by_xpath(
+                "/html/body/div[1]/div[2]/div[4]/div[3]/div[2]/div[1]/div[1]/div[1]/div[2]").text
+            target = target.split("need")[1].split()
+            balls = target[-2]
+            target = target[0]
+        except Exception:
+            target = None
+
+        body = f"{score}  Crr: {crr}"
+        if target:
+            body += f"  Target : {target}/{balls}"
+        body += f"\n{batsman1name:.12s}\t{batsman1score} ({batsman1balls} | {batsman1four} | {batsman1six} | {batsman1sr})"
+        body += f"\n{batsman2name:.12s}\t{batsman2score} ({batsman2balls} | {batsman2four} | {batsman2six} | {batsman2sr})"
+        body += f"\n{bowlername:.12s}\t{bowlerover} ({bowlerrun}-{bowlerwicket})"
+        toast.show_toast(title, body, duration=9)
+        time.sleep(15)
+except Exception:
+    pass
--- Flipkart Webscrapper.py
+++ Flipkart Webscrapper.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 text = input("Enter product name: ")
-number = int(input("Enter number of pages to scan: "))
+# number = int(input("Enter number of pages to scan: "))
 # Chrome Driver config
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
@@ -12,8 +12,8 @@ options.add_argument('--incognito')
 options.add_argument("--test-type")
 options.add_argument('--headless')
 options.add_argument('--silent')
-driver = webdriver.Chrome(".\chromedriver.exe", options=options)
-# driver = webdriver.Chrome(".\chromedriver.exe")
+driver = webdriver.Chrome(options=options)
+# driver = webdriver.Chrome()
 
 
 titles = []
@@ -21,6 +21,7 @@ prices = []
 ratings = []
 discounts = []
 links = []
+imageLinks = []
 
 
 # utility Functions
@@ -36,95 +37,97 @@ def formatDiscount(discount):
     return discount
 
 
-url = "https://www.flipkart.com/"
+url = "https://www.flipkart.com/search?q=" + text
 driver.get(url)
 # time.sleep(2)
 
-# To close login model
-driver.find_element_by_css_selector("._2AkmmA._29YdH8").click()
+# # To close login model
+# driver.find_element_by_css_selector("._2AkmmA._29YdH8").click()
 
 
-searchText = driver.find_element_by_class_name(
-    "LM6RPg")        # to locate search box
-searchText.send_keys(text)
-searchText.send_keys(Keys.ENTER)
+# searchText = driver.find_element_by_class_name(
+#     "LM6RPg")        # to locate search box
+# searchText.send_keys(text)
+# searchText.send_keys(Keys.ENTER)
 
 # time.sleep(3)
 
-pageCount = number-1
 page = driver.find_elements_by_css_selector("._3liAhj")
 
 if page:
-    while pageCount > 0:
-        for item in page:
-            print("*", end=" ")
-            title = item.find_element_by_css_selector(
-                "._2cLu-l").get_attribute("title")
-            link = item.find_element_by_css_selector(
-                "._2cLu-l").get_attribute("href")
-            price = formatPrice(
-                item.find_element_by_class_name("_1vC4OE").text)
+    for item in page:
+        print("*", end=" ")
+        title = item.find_element_by_css_selector(
+            "._2cLu-l").get_attribute("title")
+        link = item.find_element_by_css_selector(
+            "._2cLu-l").get_attribute("href")
+        imageLink = item.find_element_by_css_selector(
+            "._1Nyybr._30XEf0").get_attribute("src")
+        price = formatPrice(
+            item.find_element_by_class_name("_1vC4OE").text)
 
-            try:
-                discount = formatDiscount(item.find_element_by_class_name(
-                    "VGWI6T").find_element_by_tag_name("span").text)
-            except Exception:
-                discount = 0
+        try:
+            discount = formatDiscount(item.find_element_by_class_name(
+                "VGWI6T").find_element_by_tag_name("span").text)
+        except Exception:
+            discount = 0
 
-            try:
-                rating = item.find_element_by_class_name("hGSR34").text
-            except Exception:
-                rating = 'NA'
+        try:
+            rating = item.find_element_by_class_name("hGSR34").text
+        except Exception:
+            rating = 'NA'
 
-            titles.append(title)
-            prices.append(price)
-            ratings.append(rating)
-            discounts.append(discount)
-            links.append(link)
-        pageCount -= 1
-        link = (driver.current_url).split("&page=")[0]
-        driver.get(link + "&page=" + str(number-pageCount))
-        page = driver.find_elements_by_css_selector("._3liAhj")
-        time.sleep(2)
+        titles.append(title)
+        prices.append(price)
+        ratings.append(rating)
+        discounts.append(discount)
+        links.append(link)
+        imageLinks.append(imageLink)
+    link = (driver.current_url).split("&page=")[0]
+    page = driver.find_elements_by_css_selector("._3liAhj")
 else:
     page = driver.find_elements_by_css_selector("._1UoZlX")
-    while pageCount > 0:
-        for item in page:
-            print("*", end=" ")
-            link = item.find_element_by_css_selector(
-                "._31qSD5").get_attribute("href")
-            title = item.find_element_by_class_name("_3wU53n").text
-            price = formatPrice(
-                item.find_element_by_class_name("_1vC4OE").text)
+    for item in page:
+        print("*", end=" ")
+        link = item.find_element_by_css_selector(
+            "._31qSD5").get_attribute("href")
+        title = item.find_element_by_class_name("_3wU53n").text
+        price = formatPrice(
+            item.find_element_by_class_name("_1vC4OE").text)
 
-            try:
-                discount = formatDiscount(item.find_element_by_class_name(
-                    "VGWI6T").find_element_by_tag_name("span").text)
-            except Exception:
-                discount = 0
+        try:
+            imageLink = item.find_element_by_css_selector(
+                "._1Nyybr._30XEf0").get_attribute("src")
 
-            try:
-                rating = item.find_element_by_class_name("hGSR34").text
-            except Exception:
-                rating = 'NA'
+        except Exception:
+            imageLink = " "
 
-            titles.append(title)
-            prices.append(price)
-            ratings.append(rating)
-            discounts.append(discount)
-            links.append(link)
+        try:
+            discount = formatDiscount(item.find_element_by_class_name(
+                "VGWI6T").find_element_by_tag_name("span").text)
+        except Exception:
+            discount = 0
 
-        pageCount -= 1
-        link = (driver.current_url).split("&page=")[0]
-        driver.get(link + "&page=" + str(number-pageCount))
-        page = driver.find_elements_by_css_selector("._1UoZlX")
-        time.sleep(2)
+        try:
+            rating = item.find_element_by_class_name("hGSR34").text
+        except Exception:
+            rating = 'NA'
+
+        titles.append(title)
+        prices.append(price)
+        ratings.append(rating)
+        discounts.append(discount)
+        links.append(link)
+        imageLinks.append(imageLink)
+
+    link = (driver.current_url).split("&page=")[0]
+    page = driver.find_elements_by_css_selector("._1UoZlX")
 print()
 
 
 # To convert data into csv
 df = pd.DataFrame({'Product Name': titles, 'Prices': prices,
-                   'Rating': ratings, 'Discount %': discounts, 'Link': links})
+                   'Rating': ratings, 'Discount %': discounts, 'Link': links, "Image Links": imageLinks})
 text = text + '.csv'
 df.to_csv(text, index=False, encoding='utf-8')
 print("Check ", text)
